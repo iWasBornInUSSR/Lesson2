7d76b13596ae69989776a31d1333d02503f15e7a
Nick_PC <sshedl22@gmail.com>
2018-10-06 16:50:45 +0300

    Deleted

--- cmake-build-debug/CMakeFiles/Lesson2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Lesson2.
-# This may be replaced when dependencies are built.
---
+++ source/Set.h
@@ -0,0 +1,8 @@
+//
+// Created by Никита on 06.10.2018.
+//
+
+#ifndef LESSON2_SET_H
+#define LESSON2_SET_H
+
+#endif //LESSON2_SET_H
